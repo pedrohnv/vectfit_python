954a94222330c948483a7129ef0a55fbf1a638ba
pedrohnv <pedrohnv@hotmail.com>
2019-01-18 17:37:51 -0200

    Delete legacy vectfit. Make vectfit_2 the only one.

--- vectfit.py
+++ vectfit.py
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
 """
-Duplication of the vector fitting algorithm in python (http://www.sintef.no/Projectweb/VECTFIT/)
+@author: Phil Reinhold
 
-All credit goes to Bjorn Gustavsen for his MATLAB implementation, and the following papers
+Duplication of the vector fitting algorithm in python
+(http://www.sintef.no/Projectweb/VECTFIT/)
 
+All credit goes to Bjorn Gustavsen for his MATLAB implementation,
+and the following papers:
 
  [1] B. Gustavsen and A. Semlyen, "Rational approximation of frequency
      domain responses by Vector Fitting", IEEE Trans. Power Delivery,
@@ -16,78 +20,167 @@ All credit goes to Bjorn Gustavsen for his MATLAB implementation, and the follow
      "Macromodeling of Multiport Systems Using a Fast Implementation of
      the Vector Fitting Method", IEEE Microwave and Wireless Components
      Letters, vol. 18, no. 6, pp. 383-385, June 2008.
+
+Version 2 is a modification mainly of naming, code organization
+and documentation by Pedro H. N. Vieira.
+
+A warning about Ill conditioning of the problem may arise. To ignore
+it in your code use
+
+```
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    fitted = vector_fitting(f,s)
+```
 """
-__author__ = 'Phil Reinhold'
-from pylab import *
-from numpy.linalg import eigvals, lstsq
+import numpy
+import matplotlib.pyplot as plt
+import warnings
 
-def cc(z):
-    return z.conjugate()
-
-def model(s, poles, residues, d, h):
-    return sum(r/(s-p) for p, r in zip(poles, residues)) + d + s*h
-
-def vectfit_step(f, s, poles):
+def rational_model(s, poles, residues, d, h):
     """
-    f = complex data to fit
-    s = j*frequency
-    poles = initial poles guess
-        note: All complex poles must come in sequential complex conjugate pairs
-    returns adjusted poles
+    Complex rational function.
+
+    Parameters
+    ----------
+    s : array of complex frequencies.
+    poles : array of the pn
+    residues : array of the rn
+    d : real, offset
+    h : real, slope
+
+    Returns
+    -------
+     N
+    ----
+    \       rn
+     \   ------- + d + s*h
+     /    s - pn
+    /
+    ----
+    n=1
+    """
+    f = lambda x: (residues/(x - poles)).sum() + d + x*h
+    y = numpy.vectorize(f)
+    return y(s)
+
+def flag_poles(poles, Ns):
+    """
+    Identifies a given pole:
+        0 : real
+        1 : complex
+        2 : complex.conjugate()
+
+    Parameters
+    ----------
+    poles : initial poles guess
+        note: All complex poles must come in sequential complex
+        conjugate pairs
+    Ns : number of samples being used (s.size)
+
+    Returns
+    -------
+    cindex : identifying array
     """
     N = len(poles)
-    Ns = len(s)
-
-    cindex = zeros(N)
-    # cindex is:
-    #   - 0 for real poles
-    #   - 1 for the first of a complex-conjugate pair
-    #   - 2 for the second of a cc pair
+    cindex = numpy.zeros(N)
     for i, p in enumerate(poles):
         if p.imag != 0:
             if i == 0 or cindex[i-1] != 1:
-                assert cc(poles[i]) == poles[i+1], ("Complex poles must come in conjugate pairs: %s, %s" % (poles[i], poles[i+1]))
+                assert poles[i].conjugate() == poles[i+1], (
+                        "Complex poles"" must come in conjugate "
+                        +"pairs: %s, %s" % (poles[i], poles[i+1]))
                 cindex[i] = 1
             else:
                 cindex[i] = 2
 
-    # First linear equation to solve. See Appendix A
-    A = zeros((Ns, 2*N+2), dtype=np.complex64)
+    return cindex
+
+def residues_equation(f, s, poles, cindex, sigma_residues=True):
+    """
+    Builds the first linear equation to solve. See Appendix A.
+
+    Parameters
+    ----------
+    f : array of the complex data to fit
+    s : complex sampling points of f
+    poles : initial poles guess
+        note: All complex poles must come in sequential complex
+        conjugate pairs
+    cindex : identifying array of the poles (real or complex)
+    f_residues : bool, default=True
+        signals if the residues of sigma (True) or f (False) are being
+        calculated. The equation is a bit different in each case.
+
+    Returns
+    -------
+    A, b : of the equation Ax = b
+    """
+    Ns = len(s)
+    N = len(poles)
+    A = numpy.zeros((Ns, 2*N+2), dtype=numpy.complex64)
     for i, p in enumerate(poles):
         if cindex[i] == 0:
             A[:, i] = 1/(s - p)
         elif cindex[i] == 1:
-            A[:, i] = 1/(s - p) + 1/(s - cc(p))
+            A[:, i] = 1/(s - p) + 1/(s - p.conjugate())
         elif cindex[i] == 2:
-            A[:, i] = 1j/(s - p) - 1j/(s - cc(p))
+            A[:, i] = 1j/(s - p) - 1j/(s - p.conjugate())
         else:
             raise RuntimeError("cindex[%s] = %s" % (i, cindex[i]))
 
-        A [:, N+2+i] = -A[:, i] * f
+        if sigma_residues:
+            A[:, N+2+i] = -A[:, i]*f
 
     A[:, N] = 1
     A[:, N+1] = s
 
-    # Solve Ax == b using pseudo-inverse
     b = f
-    A = vstack((real(A), imag(A)))
-    b = concatenate((real(b), imag(b)))
-    x, residuals, rnk, s = lstsq(A, b, rcond=-1)
+    A = numpy.vstack((A.real, A.imag))
+    b = numpy.concatenate((b.real, b.imag))
+    cA = numpy.linalg.cond(A)
+    if cA > 1e13:
+        message = ('Ill Conditioned Matrix. Cond(A) = ' + str(cA)
+                    + ' . Consider scaling the problem down.')
+        warnings.warn(message, UserWarning)
+    return A, b
 
-    residues = x[:N]
-    d = x[N]
-    h = x[N+1]
+def fitting_poles(f, s, poles):
+    """
+    Calculates the poles of the fitting function.
+
+    Parameters
+    ----------
+    f : array of the complex data to fit
+    s : complex sampling points of f
+    poles : initial poles guess
+        note: All complex poles must come in sequential complex
+        conjugate pairs
+
+    Returns
+    -------
+    new_poles : adjusted poles
+    """
+    N = len(poles)
+    Ns = len(s)
+    cindex = flag_poles(poles, Ns)
+
+    # calculates the residues of sigma
+    A, b = residues_equation(f, s, poles, cindex)
+    # Solve Ax == b using pseudo-inverse
+    x, residuals, rnk, s = numpy.linalg.lstsq(A, b, rcond=-1)
 
     # We only want the "tilde" part in (A.4)
     x = x[-N:]
 
-    # Calculation of zeros: Appendix B
-    A = diag(poles)
-    b = ones(N)
+    # Calculation of zeros of sigma, which are equal to the poles
+    # of the fitting function: Appendix B
+    A = numpy.diag(poles)
+    b = numpy.ones(N)
     c = x
     for i, (ci, p) in enumerate(zip(cindex, poles)):
         if ci == 1:
-            x, y = real(p), imag(p)
+            x, y = p.real, p.imag
             A[i, i] = A[i+1, i+1] = x
             A[i, i+1] = -y
             A[i+1, i] = y
@@ -96,53 +189,41 @@ def vectfit_step(f, s, poles):
             #cv = c[i]
             #c[i,i+1] = real(cv), imag(cv)
 
-    H = A - outer(b, c)
-    H = real(H)
-    new_poles = sort(eigvals(H))
-    unstable = real(new_poles) > 0
-    new_poles[unstable] -= 2*real(new_poles)[unstable]
+    H = A - numpy.outer(b, c)
+    H = H.real
+    eig = numpy.linalg.eigvals(H)
+    new_poles = numpy.sort(eig)
+    unstable = new_poles.real > 0
+    new_poles[unstable] -= 2*new_poles.real[unstable]
     return new_poles
 
-# Dear gods of coding style, I sincerely apologize for the following copy/paste
-def calculate_residues(f, s, poles, rcond=-1):
-    Ns = len(s)
+def fitting_residues(f, s, poles):
+    """
+    Calculates the poles of the fitting function.
+
+    Parameters
+    ----------
+    f : array of the complex data to fit
+    s : complex sampling points of f
+    poles : calculated poles (by fitting _poles)
+
+    Returns
+    -------
+    residues : adjusted residues
+    d : adjusted offset
+    h : adjusted slope
+    """
     N = len(poles)
+    Ns = len(s)
+    cindex = flag_poles(poles, Ns)
 
-    cindex = zeros(N)
-    for i, p in enumerate(poles):
-        if p.imag != 0:
-            if i == 0 or cindex[i-1] != 1:
-                assert cc(poles[i]) == poles[i+1], ("Complex poles must come in conjugate pairs: %s, %s" % poles[i:i+1])
-                cindex[i] = 1
-            else:
-                cindex[i] = 2
-
-    # use the new poles to extract the residues
-    A = zeros((Ns, N+2), dtype=np.complex128)
-    for i, p in enumerate(poles):
-        if cindex[i] == 0:
-            A[:, i] = 1/(s - p)
-        elif cindex[i] == 1:
-            A[:, i] = 1/(s - p) + 1/(s - cc(p))
-        elif cindex[i] == 2:
-            A[:, i] = 1j/(s - p) - 1j/(s - cc(p))
-        else:
-            raise RuntimeError("cindex[%s] = %s" % (i, cindex[i]))
-
-    A[:, N] = 1
-    A[:, N+1] = s
+    # calculates the residues of sigma
+    A, b = residues_equation(f, s, poles, cindex, False)
     # Solve Ax == b using pseudo-inverse
-    b = f
-    A = vstack((real(A), imag(A)))
-    b = concatenate((real(b), imag(b)))
-    cA = np.linalg.cond(A)
-    if cA > 1e13:
-        print('Warning!: Ill Conditioned Matrix. Consider scaling the problem down')
-        print('Cond(A)', cA)
-    x, residuals, rnk, s = lstsq(A, b, rcond=rcond)
+    x, residuals, rnk, s = numpy.linalg.lstsq(A, b, rcond=-1)
 
     # Recover complex values
-    x = np.complex64(x)
+    x = numpy.complex64(x)
     for i, ci in enumerate(cindex):
        if ci == 1:
            r1, r2 = x[i:i+2]
@@ -154,87 +235,98 @@ def calculate_residues(f, s, poles, rcond=-1):
     h = x[N+1].real
     return residues, d, h
 
-def print_params(poles, residues, d, h):
-    cfmt = "{0.real:g} + {0.imag:g}j"
-    print("poles: " + ", ".join(cfmt.format(p) for p in poles))
-    print("residues: " + ", ".join(cfmt.format(r) for r in residues))
-    print("offset: {:g}".format(d))
-    print("slope: {:g}".format(h))
+def vector_fitting(f, s, poles_pairs=10, loss_ratio=0.01, n_iter=3,
+                   initial_poles=None):
+    """
+    Makes the vector fitting of a complex function.
 
-def vectfit_auto(f, s, n_poles=10, n_iter=10, show=False,
-                 inc_real=False, loss_ratio=1e-2, rcond=-1, track_poles=False):
-    w = imag(s)
-    pole_locs = linspace(w[0], w[-1], n_poles+2)[1:-1]
-    lr = loss_ratio
-    init_poles = poles = concatenate([[p*(-lr + 1j), p*(-lr - 1j)] for p in pole_locs])
+    Parameters
+    ----------
+    f : array of the complex data to fit
+    s : complex sampling points of f
+    poles_pairs : optional int, default=10
+        number of conjugate pairs of the fitting function.
+        Only used if initial_poles=None
+    loss_ratio : optional float, default=0.01
+        The initial poles guess, if not given, are estimated as
+        w*(-loss_ratio + 1j)
+    n_iter : optional int, default=3
+        number of iterations to do when calculating the poles, i.e.,
+        consecutive pole fitting
+    initial_poles : optional array, default=None
+        The initial pole guess
 
-    if inc_real:
-        poles = concatenate((poles, [1]))
+    Returns
+    -------
+    fitted(s) : the fitted function with 's' as parameter
+    """
+    w = s.imag
+    if initial_poles is None:
+        beta = numpy.linspace(w[0], w[-1], poles_pairs+2)[1:-1]
+        initial_poles = numpy.array([])
+        p = numpy.array([[-loss_ratio + 1j], [-loss_ratio - 1j]])
+        for b in beta:
+            initial_poles = numpy.append(initial_poles, p*b)
 
-    poles_list = []
+    poles = initial_poles
     for _ in range(n_iter):
-        poles = vectfit_step(f, s, poles)
-        poles_list.append(poles)
+        poles = fitting_poles(f, s, poles)
 
-    residues, d, h = calculate_residues(f, s, poles, rcond=rcond)
-
-    if track_poles:
-        return poles, residues, d, h, np.array(poles_list)
-
-    print_params(poles, residues, d, h)
-    return poles, residues, d, h
-
-def vectfit_auto_rescale(f, s, **kwargs):
-    s_scale = abs(s[-1])
-    f_scale = abs(f[-1])
-    print('SCALED')
-    poles_s, residues_s, d_s, h_s = vectfit_auto(f / f_scale, s / s_scale, **kwargs)
-    poles = poles_s * s_scale
-    residues = residues_s * f_scale * s_scale
-    d = d_s * f_scale
-    h = h_s * f_scale / s_scale
-    print('UNSCALED')
-    print_params(poles, residues, d, h)
-    return poles, residues, d, h
+    residues, d, h = fitting_residues(f, s, poles)
+    fitted = lambda s: rational_model(s, poles, residues, d, h)
+    return fitted
 
 if __name__ == '__main__':
-    test_s = 1j*np.linspace(1, 1e5, 800)
-    test_poles = [
-        -4500,
-        -41000,
-        -100+5000j, -100-5000j,
-        -120+15000j, -120-15000j,
-        -3000+35000j, -3000-35000j,
-        -200+45000j, -200-45000j,
-        -1500+45000j, -1500-45000j,
-        -500+70000j, -500-70000j,
-        -1000+73000j, -1000-73000j,
-        -2000+90000j, -2000-90000j,
-    ]
-    test_residues = [
-        -3000,
-        -83000,
-        -5+7000j, -5-7000j,
-        -20+18000j, -20-18000j,
-        6000+45000j, 6000-45000j,
-        40+60000j, 40-60000j,
-        90+10000j, 90-10000j,
-        50000+80000j, 50000-80000j,
-        1000+45000j, 1000-45000j,
-        -5000+92000j, -5000-92000j
-    ]
-    test_d = .2
-    test_h = 2e-5
+    true_poles = numpy.array([-4500, -41e3,
+                              -100 + 5e3j, -100 - 5e3j,
+                              -120 + 15e3j, -120 - 15e3j,
+                              -3e3 + 35e3j, -3e3 - 35e3j,
+                              -200 + 45e3j, -200 - 45e3j,
+                              -15e2 + 45e3j, -15e2 - 45e3j,
+                              -500 + 70e3j, -500 - 70e3j,
+                              -1e3 + 73e3j, -1e3 - 73e3j,
+                              -2e3 + 90e3j, -2e3 - 90e3j],
+                             dtype=numpy.complex128)
+    true_residuals = numpy.array([-3e3, -83e3,
+                                  -5 + 7e3j, -5 - 7e3j,
+                                  -20 + 18e3j, -20 - 18e3j,
+                                  6e3 + 45e3j, 6e3 - 45e3j,
+                                  40 + 60e3j, 40 - 60e3j,
+                                  90 + 10e3j, 90 - 10e3j,
+                                  50e3 + 80e3j, 50e3 - 80e3j,
+                                  1e3 + 45e3j, 1e3 - 45e3j,
+                                  -5e3 + 92e3j, -5e3 - 92e3j],
+                                 dtype=numpy.complex128)
+    true_d = -2e-12
+    true_h = -5e-18
 
-    test_f = sum(c/(test_s - a) for c, a in zip(test_residues, test_poles))
-    test_f += test_d + test_h*test_s
-    vectfit_auto(test_f, test_s)
+    freq = numpy.logspace(0, 5, 200)
+    s = 2j*numpy.pi*freq
+    true_f = rational_model(s, true_poles, true_residuals, true_d, true_h)
 
-    poles, residues, d, h = vectfit_auto_rescale(test_f, test_s)
-    fitted = model(test_s, poles, residues, d, h)
-    figure()
-    plot(test_s.imag, test_f.real)
-    plot(test_s.imag, test_f.imag)
-    plot(test_s.imag, fitted.real)
-    plot(test_s.imag, fitted.imag)
-    show()
+    initial_poles = numpy.array([-1e-2 + 1j, -1e-2 - 1j,
+                                 -1.11e2 + 1.11e4j, -1.11e2 - 1.11e4j,
+                                 -2.22e2 + 2.22e4j, -2.22e2 - 2.22e4j,
+                                 -3.33e2 + 3.33e4j, -3.33e2 - 3.33e4j,
+                                 -4.44e2 + 4.44e4j, -4.44e2 - 4.44e4j,
+                                 -5.55e2 + 5.55e4j, -5.55e2 - 5.55e4j,
+                                 -6.66e2 + 6.66e4j, -6.66e2 - 6.66e4j,
+                                 -7.77e2 + 7.77e4j, -7.77e2 - 7.77e4j,
+                                 -8.88e2 + 8.88e4j, -8.88e2 - 8.88e4j,
+                                 -1e3 + 1e5j, -1e3 - 1e5j],
+                                dtype=numpy.complex128)
+
+#    poles = fitting_poles(test_f, s, initial_poles)
+#    residues, d, h = fitting_residues(test_f, s, poles)
+#    fitted = rational_model(s, poles, residues, d, h)
+    fitted = vector_fitting(true_f, s, initial_poles=initial_poles)
+    fitted = fitted(s)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(freq/1e3, numpy.abs(true_f))
+    ax.plot(freq/1e3, numpy.abs(fitted), 'x')
+    ax.set_xlabel("f [kHz]")
+    ax.set_ylabel("Magnitude [p.u.]")
+    ax.legend(["true", "fitted"])
+    plt.show()
