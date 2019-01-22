682e650cd4ab2ce85d24354567794dd45878d492
pedrohnv <pedrohnv@hotmail.com>
2019-01-22 14:46:30 -0200

    Turn auto_rescale off as that yields a better result. Investigate...

--- ex2.py
+++ ex2.py
@@ -86,7 +86,7 @@ for n in range(bet.size):
 # Real starting poles :
 #poles = -np.linspace(w[0], w[Ns-1], N);
 Niter = 3
-poles, residues, d, h = vector_fitting(f, s, initial_poles=poles, n_iter=Niter, auto_rescale=True)
+poles, residues, d, h = vector_fitting(f, s, initial_poles=poles, n_iter=Niter, auto_rescale=False)
 foo = np.vectorize( lambda i: rational_model(s, poles, residues[i], d[i], h[i]) )
 f1 = foo(0)
 f2 = foo(1)
